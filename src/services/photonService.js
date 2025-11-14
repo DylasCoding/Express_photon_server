30d61c7de30f0defe2041ce055a3f8c43ad4674e
DylasCoding <hieusih@gmail.com>
2025-11-14 20:19:09 +0700

    fix services 1

--- src/services/photonService.js
+++ src/services/photonService.js
@@ -1,28 +1,40 @@
-// src/services/photonService.js
 const { client, Photon } = require('../config/photon');
 
 class PhotonService {
+    constructor() {
+        this.pendingConnect = null;
+        this.pendingRoomAction = null;
+
+        client.onStateChange = (state) => this.handleStateChange(state);
+    }
+
+    handleStateChange(state) {
+        console.log("Photon state:", state);
+
+        if (this.pendingConnect &&
+            state === Photon.LoadBalancing.LoadBalancingClient.State.ConnectedToMaster) {
+            this.pendingConnect.resolve();
+            this.pendingConnect = null;
+        }
+
+        if (this.pendingConnect &&
+            state === Photon.LoadBalancing.LoadBalancingClient.State.Disconnected) {
+            this.pendingConnect.reject(new Error("Photon disconnected"));
+            this.pendingConnect = null;
+        }
+
+        if (this.pendingRoomAction &&
+            state === Photon.LoadBalancing.LoadBalancingClient.State.Joined) {
+            this.pendingRoomAction.resolve();
+            this.pendingRoomAction = null;
+        }
+    }
+
     async ensureConnected() {
         if (client.isConnectedToMaster()) return;
 
         return new Promise((resolve, reject) => {
-            const timeout = setTimeout(() => reject(new Error("Photon connect timeout")), 10000);
-
-            const oldHandler = client.onStateChange;
-
-            client.onStateChange = (state) => {
-                if (state === Photon.LoadBalancing.LoadBalancingClient.State.ConnectedToMaster) {
-                    clearTimeout(timeout);
-                    client.onStateChange = oldHandler || null;
-                    resolve();
-                }
-                else if (state === Photon.LoadBalancing.LoadBalancingClient.State.Disconnected) {
-                    clearTimeout(timeout);
-                    client.onStateChange = oldHandler || null;
-                    reject(new Error("Photon disconnected"));
-                }
-            };
-
+            this.pendingConnect = { resolve, reject };
             client.connectToRegionMaster();
         });
     }
@@ -35,60 +47,33 @@ class PhotonService {
         }
 
         return new Promise((resolve, reject) => {
-            const oldHandler = client.onStateChange;
-
-            client.onStateChange = (state) => {
-                if (state === Photon.LoadBalancing.LoadBalancingClient.State.Joined) {
-                    client.onStateChange = oldHandler || null;
-
-                    resolve({
-                        roomId: client.myRoom().name,
-                        actorNr: client.myActor().actorNr
-                    });
-                }
+            this.pendingRoomAction = {
+                resolve: () => resolve({
+                    roomId: client.myRoom().name,
+                    actorNr: client.myActor().actorNr
+                })
             };
 
             client.createRoom(roomName, {
                 maxPlayers,
                 isVisible: true,
                 isOpen: true,
-                customRoomProperties: { hostId, status: 'waiting' }
+                customRoomProperties: { hostId, status: "waiting" }
             });
         });
     }
 
-    async joinRoom(roomId, playerId) {
+    async joinRoom(roomId) {
         await this.ensureConnected();
 
         return new Promise((resolve, reject) => {
-            const onStateChange = (state) => {
-                if (state === Photon.LoadBalancing.LoadBalancingClient.State.Joined) {
-                    client.removeOnStateChange(onStateChange);
-                    resolve({
-                        success: true,
-                        actorNr: client.myActor().actorNr
-                    });
-                }
+            this.pendingRoomAction = {
+                resolve: () => resolve({
+                    actorNr: client.myActor().actorNr
+                })
             };
-
-            client.addOnStateChange(onStateChange);
             client.joinRoom(roomId);
         });
-    }
-
-    async leaveRoom() {
-        if (client.myRoom()) {
-            client.leaveRoom();
-        }
-    }
-
-    // Gửi event realtime (move, shoot,...)
-    raiseEvent(code, data) {
-        if (client.myRoom()) {
-            client.raiseEvent(code, data, {
-                receivers: Photon.LoadBalancing.Constants.ReceiverGroup.All
-            });
-        }
     }
 }
 
